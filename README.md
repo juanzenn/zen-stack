4a7ec82aa548dcf399239cc9df66696dcdb836d3
Juan Alvarez <juanandres140299@gmail.com>
2023-06-15 19:20:51 -0400

    init shadcn, configure theme, add other deps

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Zen Stack
 
 I use this temaplte/stack to build full-stack applications for personal projects and clients. It's not unique, but a great starting point for my projects.
+
+## Shoutouts to the following projects
+
+- [Next.js](https://nextjs.org/)
+- [React](https://reactjs.org/)
+- [Tailwind CSS](https://tailwindcss.com/)
+- [TypeScript](https://www.typescriptlang.org/)
+- [Shadcn](https://ui.shadcn.com/)
+- [Lucide Icons](https://lucide.dev/)
+- [Zod](https://zod.dev/)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { Github } from "lucide-react";
+
 export default function Home() {
   return (
-    <main className="bg-zinc-900 h-screen w-screen flex items-center justify-center flex-col text-zinc-400">
-      <h1 className="font-black text-6xl">Zen Stack</h1>
-      <p className="font-medium mt-4 text-lg">
+    <main className="flex h-screen w-screen flex-col items-center justify-center bg-background text-foreground">
+      <h1 className="text-6xl font-black">Zen Stack</h1>
+      <p className="mt-4 text-lg font-medium">
         A Next.js template with modern technologies.
       </p>
-
-      <a
-        href="https://github.com/juanzenn/zen-stack"
-        className="block mt-6 py-2 px-6 bg-zinc-400 text-zinc-900 font-medium rounded shadow"
-        target="_blank"
-      >
-        GitHub
-      </a>
+      <Button asChild className="mt-4" size="lg">
+        <a href="https://github.com/juanzenn/zen-stack" target="_blank">
+          <Github className="mr-2 inline-block" size={20} />
+          GitHub
+        </a>
+      </Button>
     </main>
   );
 }
